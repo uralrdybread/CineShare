91b7080c2ef1a22ba20f54f1fb5cf644e5610818
Douglas White <douglas.rw@outlook.com>
2024-04-29 21:56:04 -0600

    first commit

---
+++ resources/views/components/how.blade.php
@@ -0,0 +1,22 @@
+<div class="bg-cyan-800">
+    <div class="flex justify-center my-5">
+        <h2>How it works:</h2>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mx-5">
+
+        <div class="bg-gray-200 p-6 rounded-lg shadow-md">
+            Sign up for a Cineshare account to access our movie library.
+        </div>
+        <div class="bg-gray-200 p-6 rounded-lg shadow-md">
+            Browse our selection of available movies and choose one to borrow.
+        </div>
+        <div class="bg-gray-200 p-6 rounded-lg shadow-md">
+            Enjoy your movie at home, whether it's a solo movie night or a family gathering.
+        </div>
+        <div class="bg-gray-200 p-6 rounded-lg shadow-md">
+            Return the movie after watching and share your thoughts by leaving a comment, helping others decide on their
+            next movie.
+        </div>
+    </div>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-navbar />
+
+<x-layout>
+    <div>
+        <img src="{{ asset('images/originalhero.png') }}" alt="Description of the image" class="w-full max-h-200 h-auto">
+    </div>
+
+    <x-how />
+</x-layout>
